1ee8de5971d499f828bca32413f603b2ea4fdf68
unknown <djaredchase@gmail.com>
2020-05-19 01:09:21 -0400

    did app.ts file

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { db } from './daos/db';
+import { clientRouter } from './routers/client.router';
+import { exterminatorRouter } from './routers/exterminator.router';
+import { pestRouter } from './routers/pest.router';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+    next();
+});
+
+app.use('/client', clientRouter);
+app.use('/extermintor', exterminatorRouter);
+app.use('/pest', pestRouter);
+
+process.on('unhandledRejection', () => {
+    db.end().then(() => {
+        console.log('Database pool closed');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`)
+});
